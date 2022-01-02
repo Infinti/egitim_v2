d6a3f28bc7b43dba8990514242da4397cc833ab1
Infinti <79116441+Infinti@users.noreply.github.com>
2022-01-02 19:28:26 +0300

    Add files via upload

---
+++ egitim_v2/operatorler_pat/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace operatorler_pat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****atama-işlemli atama!*******");
+            int x = 3;
+            int y = 3;
+            y = y + 2;
+            Console.WriteLine(y);
+            y += 2;
+            Console.WriteLine(y);
+            y /= 1;
+            Console.WriteLine(y);
+            x *= 2;
+            Console.WriteLine(x);
+
+
+            Console.WriteLine("*****Mantıksal!*******");
+            bool isSuccess = true;
+            bool isCompleted = false;
+
+            if (isSuccess && isCompleted)
+                Console.WriteLine("Perfect!");
+
+            if (isSuccess || isCompleted)
+                Console.WriteLine("Great!");
+
+            if (isSuccess && !isCompleted)
+                Console.WriteLine("Fine!");
+
+            Console.WriteLine("*****İlişkisel!*******");
+
+            int a = 3;
+            int b = 4;
+            bool sonuc = a < b;
+            Console.WriteLine(sonuc);
+            sonuc = a > b;
+            Console.WriteLine(sonuc);
+            sonuc = a >= b;
+            Console.WriteLine(sonuc);
+            sonuc = a <= b;
+            Console.WriteLine(sonuc);
+            sonuc = a == b;
+            Console.WriteLine(sonuc);
+            sonuc = a != b;
+            Console.WriteLine(sonuc);
+
+            Console.WriteLine("*****Aritmetik!*******");
+
+            int sayi1 = 10;
+            int sayi2 = 5;
+            int sonuc1 = sayi1 / sayi2;
+            Console.WriteLine(sonuc1);
+
+            sonuc1 = sayi1 * sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1 + sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = ++sayi1;
+            Console.WriteLine(sonuc1);
+
+            int sonuc2 = 20 % 3;
+            Console.WriteLine(sonuc2);
+
+
+        }
+    }
+}
