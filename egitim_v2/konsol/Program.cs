e7870e48ef58cdea892c2634775b3f9135c2aa07
Infinti <79116441+Infinti@users.noreply.github.com>
2022-01-02 06:44:06 +0300

    Add files via upload

---
+++ egitim_v2/konsol/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace konsol
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("İsminizi yazınız:");
+            string name_sur = Console.ReadLine();
+            Console.WriteLine("Hoşgeldiniz " + name_sur);
+        }
+    }
+}
